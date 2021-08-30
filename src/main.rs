63db5a3515023c323c6719b3542ddc9f89c72701
Sriram G <marirs@gmail.com>
2021-08-30 11:50:28 +0530

    added subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{crate_authors, crate_version, Clap};
+use clap::{crate_authors, crate_version, App, Arg};
 use std::{
     fs::{read_to_string, File},
     io::Write,
@@ -9,88 +9,105 @@ use yara_dedup::nom::parse_vec;
 use yara_dedup::utils::{collect_yar_files, remove_comments};
 use yara::Compiler;
 
-
-#[derive(Clap)]
-#[clap(version = crate_version!(), author = crate_authors!())]
-struct CliOpts {
-    #[clap(
-        short = 'i',
-        long,
-        about = "directory containing the yara rule files for dedupe"
-    )]
-    input_dir: String,
-    #[clap(
-        short = 'o',
-        long,
-        about = "output file name to store the deduplicated single yara file"
-    )]
-    output_file: String,
-    #[clap(
-        short = 'c',
-        long,
-        about = "enabling this will give a single deduplicated compiled yara file"
-    )]
-    compile: bool,
-}
-
 fn main() {
-    let cli_opts = CliOpts::parse();
-    let input_dir = if cli_opts.input_dir.is_empty() {
-        println!("Input folder not define: {:?}", &cli_opts.input_dir);
-        exit(1)
-    } else if !Path::new(&cli_opts.input_dir).is_dir() {
-        println!(
-            "Input folder not dir or not exists: {:?}",
-            &cli_opts.input_dir
-        );
-        exit(1)
-    } else {
-        &cli_opts.input_dir
-    };
-    let output_file = &cli_opts.output_file;
-    let is_compile = cli_opts.compile;
-    let mut file_count = 0;
+    let matches = App::new("Yara Dedupe")
+        .about("Dedupes & compiles given yara rules")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .license("MIT")
+        .subcommand(
+            App::new("dedupe")
+                .about("Dedupe given yara rules")
+                .arg(
+                    Arg::new("input_dir")
+                        .about("directory containing the yara rule files for dedupe")
+                        .short('i')
+                        .long("input-dir")
+                        .required(true)
+                        .takes_value(true)
+                )
+                .arg(
+                    Arg::new("output_file")
+                        .about("output file name to store the deduplicated single yara file")
+                        .short('o')
+                        .long("output-file")
+                        .required(true)
+                        .takes_value(true)
+                ),
+        )
+        .subcommand(
+            App::new("compile")
+                .about("Compiles a given Yara ruleset")
+                .arg(
+                    Arg::new("input_file")
+                        .about("yara ruleset file to compile")
+                        .required(true)
+                )
+        )
+        .get_matches();
 
-    let mut all_yars: Vec<_> = collect_yar_files(&input_dir)
-        .into_iter()
-        .inspect(|x| {
-            print!("\r[* examining: {:120}]", x);
-            let _ = std::io::stdout().flush();
-            file_count += 1;
-        })
-        .map(|path| read_to_string(&path).unwrap())
-        .map(remove_comments)
-        .flat_map(|x| parse_vec(&x).map(|x| x.1).ok())
-        .flatten()
-        .collect();
+    match matches.subcommand() {
+        Some(("dedupe", dedupe_args)) => {
+            let input_dir = dedupe_args.value_of("input_dir").unwrap();
+            let input_dir = if input_dir.is_empty() {
+                println!("Input folder not define: {:?}", input_dir);
+                exit(1)
+            } else if !Path::new(input_dir).is_dir() {
+                println!("Input folder not dir or not exists: {:?}", input_dir);
+                exit(1)
+            } else {
+                input_dir
+            };
+            let output_file = dedupe_args.value_of("output_file").unwrap();
 
-    println!();
-    println!("* Total files processed: {}", file_count);
-    println!("* Total yara rules: {}", all_yars.len());
+            let mut file_count = 0;
 
-    all_yars.sort_by_key(|x| x.name.clone());
-    all_yars.dedup_by_key(|x| x.name.clone());
+            let mut all_yars: Vec<_> = collect_yar_files(&input_dir)
+                .into_iter()
+                .inspect(|x| {
+                    print!("\r[* examining: {:120}]", x);
+                    let _ = std::io::stdout().flush();
+                    file_count += 1;
+                })
+                .map(|path| read_to_string(&path).unwrap())
+                .map(remove_comments)
+                .flat_map(|x| parse_vec(&x).map(|x| x.1).ok())
+                .flatten()
+                .collect();
 
-    println!("* Total yara rules after dedupe: {}", all_yars.len());
+            println!();
+            println!("* Total files processed: {}", file_count);
+            println!("* Total yara rules: {}", all_yars.len());
 
-    File::create(output_file)
-        .map(|mut f| {
-            for e in all_yars {
-                write!(f, "{}\n\n", e.to_string()).expect("error")
-            }
-        })
-        .expect("error");
+            all_yars.sort_by_key(|x| x.name.clone());
+            all_yars.dedup_by_key(|x| x.name.clone());
 
-    println!("* Output yara file stored in: {}", output_file);
+            println!("* Total yara rules after dedupe: {}", all_yars.len());
 
-    if is_compile {
-        let mut compiler = Compiler::new().unwrap();
-        compiler.add_rules_file(output_file).unwrap();
-        let compiled_output_file = format!("compiled_{}", output_file);
-        let mut rules = compiler.compile_rules().expect("Couldn't compile the rules");
-        rules.save(&compiled_output_file).expect("Couldn't save the compiled rules");
+            File::create(output_file)
+                .map(|mut f| {
+                    for e in all_yars {
+                        write!(f, "{}\n\n", e.to_string()).expect("error")
+                    }
+                })
+                .expect("error");
 
-        println!("* Compiled yara ruleset is stored in: {}", compiled_output_file);
+            println!("* Output yara file stored in: {}", output_file);
+
+        },
+        Some(("compile", compile_args)) => {
+            let input_file = compile_args.value_of("input_file").unwrap();
+            let mut compiler = Compiler::new().unwrap();
+            compiler.add_rules_file(input_file).unwrap();
+            let compiled_output_file = format!("compiled_{}", input_file);
+            let mut rules = compiler.compile_rules().expect("Couldn't compile the rules");
+            rules.save(&compiled_output_file).expect("Couldn't save the compiled rules");
+
+            println!("* Compiled yara ruleset is stored in: {}", compiled_output_file);
+
+        },
+        None => println!("No command passed. Nothing to do."),
+        _ => unreachable!()
     }
 
     println!();
